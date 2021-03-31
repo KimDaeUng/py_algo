49d9f6c619128a1452d199ab966cb73eea167630
KimDaeUng <wungi123@naver.com>
2021-03-31 13:51:36 +0900

    solved: 16-32.정수-삼각형

--- New_Book/16-32.정수-삼각형.py
+++ New_Book/16-32.정수-삼각형.py
@@ -6,6 +6,7 @@
 a[r][c] = a[r][c] + max(a[r-1][c-1], a[r-1][c])
 '''
 
+# My Solution
 n = int(input())
 data = []
 for i in range(n):
@@ -15,3 +16,29 @@ for r in range(1, n):
     for c in range(1, len(data[r])-1):
         data[r][c] = data[r][c] + max(data[r-1][c-1], data[r-1][c])
 print(max(data[r]))
+
+
+# Solution
+n = int(input())
+dp = [] # dp table initialization
+
+for _ in range(n):
+    dp.append(list(map(int, input().split())))
+
+# DP로 두 번째 줄부터 내려가면서 확인
+for i in range(1, n):
+    for j in range(i + 1):
+        # 왼쪽 위에서 내려오는 경우
+        if j == 0:
+            up_left = 0
+        else:
+            up_left = dp[i - 1][j - 1]
+        # 바로 위에서 내려오는 경우
+        if j == i:
+            up = 0
+        else:
+            up = dp[i - 1][j]
+        # 최대 합을 저장
+        dp[i][j] = dp[i][j] + max(up_left, up)
+
+print(max(dp[n - 1]))
