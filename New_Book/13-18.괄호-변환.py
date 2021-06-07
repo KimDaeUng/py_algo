dce787f2e4c43044a3d34c0c73c799894a73f6d1
KimDaeUng <wungi123@naver.com>
2021-06-07 11:21:14 +0900

    fix: 13-18.괄호-변환 add solution

--- New_Book/13-18.괄호-변환.py
+++ New_Book/13-18.괄호-변환.py
@@ -15,6 +15,7 @@
 #         else:
 #             return False
 
+# My Solution
 def is_balance(s):
     nl, nr = 0, 0
     for i in s:
@@ -74,43 +75,48 @@ p = "(()())()"
 print(solution(p))
 
 # Solution
+# '균형잡힌 괄호 문자열'의 인덱스 반환
+def balanced_idx(p):
+    c = 0 # '('의 개수
+    for i in range(len(p)):
+        if p[i] == '(':
+            c += 1
+        else:
+            c -= 1
+        if c == 0:
+            return i
 
-# # '균형잡힌 괄호 문자열'의 인덱스 반환
-# def balanced_idx(p):
-#     c = 0 # '('의 개수
-#     for i in range(len(p)):
-#         if p[i] == '(':
-#             c += 1
-#         else:
-#             c -= 1
-#         if c == 0:
-#             return i
+# '올바른 괄호 문자열'인지 판단
+def check_sound(p):
+    c = 0 # '('의 개수
+    for i in p:
+        if i == '(':
+            c += 1
+        else:
+            # 쌍이 맞지 않는 경우에 False 반환
+            if c == 0:
+                return False
+            c -= 1
+    return True # 쌍이 맞는 경우 True 반환
 
-# # '올바른 괄호 문자열'인지 판단
-# def check_sound(p):
-#     c = 0 # '('의 개수
-#     for i in p:
-#         if i == '(':
-#             c += 1
-#         else:
-#             # 쌍이 맞지 않는 경우에 False 반환
-#             if c == 0:
-#                 return False
-#             c -= 1
-#     return True # 쌍이 맞는 경우 True 반환
-
-# def solution(p):
-#     answer = ''
-#     if p == '':
-#         return answer
-#     idx = balanced_idx(p)
-#     u = p[:idx + 1]
-#     v = p[idx + 1:]
-#     # '올바른 괄호 문자열'이면, v에 대해 함수를 수행한 결과를 붙여 반환
-#     if check_sound(u):
-#         answer = u + solution(v)
-#     # '올바른 괄호 문자열'이 아니라면 아래 과정을 수행
-#     else:
-#         answer = '(' + solution(v) + ')'
-#         u = list(u[1:-1])
-#         for i in range(len(u)):
+def solution(p):
+    answer = ''
+    if p == '':
+        return answer
+    idx = balanced_idx(p)
+    u = p[:idx + 1]
+    v = p[idx + 1:]
+    # '올바른 괄호 문자열'이면, v에 대해 함수를 수행한 결과를 붙여 반환
+    if check_sound(u):
+        answer = u + solution(v)
+    # '올바른 괄호 문자열'이 아니라면 아래 과정을 수행
+    else:
+        answer = '(' + solution(v) + ')'
+        u = list(u[1:-1]) # 첫 번째와 마지막 문자를 제거
+        for i in range(len(u)):
+            if u[i] == '(':
+                u[i] = ')'
+            else:
+                u[i] = '('
+        answer += "".join(u)
+    return answer
