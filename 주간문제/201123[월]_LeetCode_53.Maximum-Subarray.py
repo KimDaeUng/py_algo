fdcdfc09a3b66790214615f3058a77cf38b5d24e
KimDaeUng <wungi123@naver.com>
2021-06-17 17:15:50 +0900

    fix: add comment on leetcode_53

--- 주간문제/201123[월]_LeetCode_53.Maximum-Subarray.py
+++ 주간문제/201123[월]_LeetCode_53.Maximum-Subarray.py
@@ -1,5 +1,12 @@
 # https://leetcode.com/problems/maximum-subarray/
-
+'''
+DP 문제
+입력받은 배열을 그대로 복사하여 DP 테이블 만든 후,
+dp[i] : i번째까지의 연속된 합의 최대값
+매번 현재까지의 연속된 합의 최대값과, 현재값과 비교하여 현재값이 더 크다면
+그 지점부터 현재값으로 새로 갱신함
+dp[i] = max(dp[i-1] + nums[i], nums[i])
+'''
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         cache = nums[:]
